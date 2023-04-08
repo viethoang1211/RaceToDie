b7e31e7f84434fc4ba1d4e29818b8627a43ec4a6
vhdat <81861683+vhdat@users.noreply.github.com>
2023-04-08 19:52:18 +0700

    update game UI

--- main.cpp
+++ main.cpp
@@ -27,12 +27,13 @@
 using namespace std;
 
 
+
 class Textbox : public sf::Drawable, public sf::Transformable {
 public:
 	Textbox(unsigned int len) :
 			max_len(len),
             my_rect(sf::Vector2f(30 * max_len, 40)), 
-            my_focus(false)
+            my_focus(true)
         {
             my_font.loadFromFile("C:/Windows/Fonts/Arial.ttf"); // change later
 			my_text.setFont(my_font);
@@ -60,7 +61,7 @@ public:
 			text = text.substr(0, text.size() - 1);
 			my_text.setString(text+ "_");
 		}
-		else if (text.size() < max_len){
+		else if (text.size() < max_len && e.text.unicode != 13){
 			text+= e.text.unicode;
 			my_text.setString(text+ "_");
 		}
@@ -106,47 +107,79 @@ private:
 	sf::RectangleShape my_rect;
 	bool my_focus;
 };
-
-int main() {
-	sf::RenderWindow window;
-
-	window.create(sf::VideoMode(1200, 900), "SFML Textbox");
-	
-	sf::Font f;
-	f.loadFromFile("C:/Windows/Fonts/Arial.ttf");
-	sf::Text my_text2;
-	my_text2.setFont(f);
-	my_text2.setColor(sf::Color::White);
-	my_text2.setPosition(0,0);
-	
+string game_input_name(sf::RenderWindow& window){
 	Textbox tb1(20);
 	tb1.setPosition(100,100);
-	//Main Loop:
+	sf::Event event;
 	while (window.isOpen()) {
-
-		sf::Event event;
 		while (window.pollEvent(event)){
-            if (event.type == sf::Event::Closed)
-                window.close();
-            else if (event.type == sf::Event::MouseButtonReleased){
-                auto pos = sf::Mouse::getPosition(window);
-                tb1.setFocus(false);
-                if (tb1.contains(sf::Vector2f(pos))){
-                    tb1.setFocus(true);
-                }
-            }else{
-                tb1.input(event);
-				my_text2.setString(tb1.getString());
-				if(tb1.getString()=="helllo")
-					window.close();
-				if(my_text2.getString()=="dat")
-					my_text2.setString("bulll");
-            }
+			if (event.type == sf::Event::Closed)
+				window.close();
+			else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)){
+				return tb1.getString();
+			}	
+			// else if (event.type == sf::Event::MouseButtonReleased){
+			// 	auto pos = sf::Mouse::getPosition(window);
+			// 	tb1.setFocus(false);
+			// 	if (tb1.contains(sf::Vector2f(pos))){
+			// 		tb1.setFocus(true);
+			// 	}
+			// }
+			else{
+				tb1.input(event);
+			}
 		}
-            window.clear();
-			window.draw(my_text2);
-            window.draw(tb1);
-            window.display();
+		window.clear();
+		window.draw(tb1);
+		window.display();
 	}
+	return "";
+}
+int game(sf::RenderWindow& window){
+	sf::Image image;
+	image.loadFromFile("obs3.png");
+	image.createMaskFromColor(sf::Color::White);
+	sf::Texture car;
+	car.loadFromImage(image);
+	sf::Sprite cars(car);
+	
+	vector<sf::Sprite> carss(10,cars);
+	for(int i=0;i<10;i++){
+		carss[i].setPosition(15+105*i,825-120);
+	}
+
+	sf::Texture race;
+	if(!race.loadFromFile("a.png"))
+		return EXIT_FAILURE;
+	sf::Sprite	racing;
+	racing.setTexture(race);
+
+	sf::Event event;
+	while (window.isOpen()) {
+		while (window.pollEvent(event)){
+			if (event.type == sf::Event::Closed)
+				window.close();
+			else if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
+				window.close();
+			}	
+		}
+		window.clear();
+		for(int i=0;i<10;i++){
+			racing.setPosition(105*i,0);
+			window.draw(racing);
+			window.draw(carss[i]);
+		}
+		window.display();
+	}
+	return 0;
+}
+int main() {
+	sf::RenderWindow window;
+	window.create(sf::VideoMode(1200, 900), "SFML Textbox");
+
+	if(game_input_name(window)=="testt"){
+		return game(window);
+	};    
+
 	return 0;
 }
