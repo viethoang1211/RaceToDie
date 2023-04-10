45ea79f82d319e094b71bd8f7630ad1dbdcb7b65
vietAPCS <87920454+vietAPCS@users.noreply.github.com>
2023-04-10 15:39:07 +0000

    Pending changes exported from your codespace

--- hellow.cpp
+++ hellow.cpp
@@ -2,23 +2,26 @@
 #include <string>
 #include <cstring>
 using namespace std;
-
- int main(void) {
-   char buffer[50];
-   char buffer1[50];
-   char buffer2[50];
-   int num = 002;
-   int num2 = 123456;
-   int num3 = 898;
-   if(num<9)
-   int len = snprintf(buffer, sizeof(buffer), "0%d", num);
-   else
-   int len = snprintf(buffer, sizeof(buffer), "%d", num);
-   cout << atoi(buffer) << endl;
-   snprintf(buffer2, sizeof(buffer2), "%d", num3);
-   strcat(buffer, buffer2);
-   snprintf(buffer1, sizeof(buffer1), " and it's twice %d", num*2);
-   strcat(buffer, buffer1);
-   cout << buffer;
-   return 0;
- }
+int getRandomInt(int min, int max) {
+    return rand() % (max - min + 1) + min;
+}
+int main(void) {
+  //  char buffer[50];
+  //  char buffer1[50];
+  //  char buffer2[50];
+  //  int num = 002;
+  //  int num2 = 123456;
+  //  int num3 = 898;
+  //  if(num<9)
+  //  int len = snprintf(buffer, sizeof(buffer), "0%d", num);
+  //  else
+  //  int len = snprintf(buffer, sizeof(buffer), "%d", num);
+  //  cout << atoi(buffer) << endl;
+  //  snprintf(buffer2, sizeof(buffer2), "%d", num3);
+  //  strcat(buffer, buffer2);
+  //  snprintf(buffer1, sizeof(buffer1), " and it's twice %d", num*2);
+  //  strcat(buffer, buffer1);
+  //  cout << buffer;
+  cout << getRandomInt(3,9);
+  return 0;
+}
